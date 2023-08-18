06cd2c29e152cefe84d0c8e4190cac8d3d27c86b
fitrahfajar14 <108163447+fitrahfajar14@users.noreply.github.com>
2023-08-18 18:59:36 +0700

    Update ultrasonik.py

--- app.py
+++ app.py
@@ -1,27 +1,75 @@
-from ultrasonic import distance
-import requests
 import time
+import requests
+import math
+import random
+import time
+import RPi.GPIO as GPIO
 
-# ultrasonic 1
-gpio_echo_1 = 1
-gpio_trigger_1 = 2
-device_label_1 = 'ultrasonic_1'
-device_token_1 = 'BBFF-xPHGGSOJUxeuh8Ijbz39FYaTdZFFSd'
+TOKEN = "BBFF-llFcdHmn0AYFcGp7O0D0Uisu6EfvPo"   
+DEVICE_LABEL = "HOCAGEE_44"  
+VARIABLE_LABEL_1 = "sensor_ultrasonic"  
 
-# ultrasonic 2
-gpio_echo_2 = 4
-gpio_trigger_2 = 5
-device_label_2 = 'ultrasonic_2'
-device_token_2 = 'BBFF-lTM2PEeMrd9fpm1Y2uITnxUUUhtTqg'
+GPIO.setmode(GPIO.BCM)  
+GPIO.setwarnings(False)
+ 
+
+GPIO_TRIGGER = 18 
+GPIO_ECHO = 24 
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)
+GPIO.setup(GPIO_ECHO,GPIO.IN)  
+ 
+
+GPIO.output(GPIO_TRIGGER, GPIO.LOW)
 
 
-def send_to_ubidots(device_label, device_token, payload):
-    # Creates the headers for the HTTP requests
+def get_range():
+    
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+     
+    
+    GPIO.output(GPIO_TRIGGER, False)
+    timeout_counter = int(time.time())
+    start = time.time()
+ 
+    
+    while GPIO.input(GPIO_ECHO)==0 and (int(time.time()) - timeout_counter) < 3:
+        start = time.time()
+ 
+    timeout_counter = int(time.time())
+    stop = time.time()
+    
+    while GPIO.input(GPIO_ECHO)==1 and (int(time.time()) - timeout_counter) < 3:
+        stop = time.time()
+ 
+    
+    elapsed = stop-start
+ 
+    
+    distance = elapsed * 34320
+ 
+    
+    distance = distance / 2
+ 
+    
+    return distance
+
+
+def build_payload(variable_1):
+    
+    val = get_range()
+    value_1 = float("{0:.2f}".format(val))
+    payload = {variable_1: value_1}
+
+    return payload
+
+
+def post_request(payload):
+    
     url = "http://industrial.api.ubidots.com"
-    url = "{}/api/v1.6/devices/{}".format(url, device_label)
-    headers = {"X-Auth-Token": device_token, "Content-Type": "application/json"}
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
 
-    # Makes the HTTP requests
     status = 400
     attempts = 0
     while status >= 400 and attempts <= 5:
@@ -30,34 +78,24 @@ def send_to_ubidots(device_label, device_token, payload):
         attempts += 1
         time.sleep(1)
 
-    # Processes results
     print(req.status_code, req.json())
     if status >= 400:
-        print("[ERROR] Could not send data after 5 attempts, please check \
-            your token credentials and internet connection")
+        print("[ERROR] Could not send data after 5 attempts, please check your token credentials and internet connection")
         return False
 
     print("[INFO] request made properly, your device is updated")
     return True
 
 
+def main():
+    payload = build_payload(VARIABLE_LABEL_1)
 
-if __name__ == "__main__":
-    while True:
-        # sensor 1 reading
-        distance_1 = distance(gpio_trigger_1, gpio_echo_1)
-        distance_1_payload = {
-            'ultrasonic': distance_1
-        }
-        send_to_ubidots(device_label_1, device_token_1, distance_1_payload)
-        
-        # tambahan logic misal jarak tertentu menyalakan/matikan relay atau servo
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
 
-        # sensor 2 reading
-        distance_2 = distance(gpio_trigger_2, gpio_echo_2)
-        distance_2_payload = {
-            'ultrasonic': distance_2
-        }
-        send_to_ubidots(device_label_2, device_token_2, distance_2_payload)
 
-        # tambahan logic misal jarak tertentu menyalakan/matikan relay atau servo
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
